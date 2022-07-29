9d69b3540d9613dba4367f7754cf3b5c5e0f8f4f
lipengpeng <lipurpose@aliyun.com>
2022-07-29 17:31:27 +0800

    update unity 测试

---
+++ Assets/Scripts/ScreenFit.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFit : MonoBehaviour
+{
+    [SerializeField]
+    Image bg;
+
+    CanvasScaler scaler;
+    RectTransform canvasRect;
+    
+
+    private void OnEnable()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaler = this.GetComponent<CanvasScaler>();
+        Debug.Log("屏幕大小: " + Screen.width + " " + Screen.height);
+        RectTransform rectTransform = bg.GetComponent<RectTransform>();
+        canvasRect = this.GetComponent<RectTransform>();
+
+        if ((float)(Screen.width)/Screen.height > scaler.referenceResolution.x / scaler.referenceResolution.y)
+        {
+            scaler.matchWidthOrHeight = 1.0f;
+            float scale = (Screen.width / scaler.referenceResolution.x) / (Screen.height / rectTransform.sizeDelta.y);
+            rectTransform.localScale = new Vector3(scale, scale, scale);
+        }
+        else
+        {
+            scaler.matchWidthOrHeight = 0.0f;
+            float scale = (Screen.height / scaler.referenceResolution.y) / (Screen.width / rectTransform.sizeDelta.x);
+            rectTransform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
