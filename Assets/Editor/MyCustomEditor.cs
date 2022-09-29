e3d801b34f97be14b2b02f61e68670f0cb800db8
lipengpeng <lipurpose@aliyun.com>
2022-09-29 11:22:47 +0800

    update 提交修改修改

---
+++ Assets/Editor/MyCustomEditor.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class MyCustomEditor : EditorWindow
+{
+    [MenuItem("Window/UI Toolkit/MyCustomEditor")]
+    public static void ShowExample()
+    {
+        MyCustomEditor wnd = GetWindow<MyCustomEditor>();
+        wnd.titleContent = new GUIContent("MyCustomEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+
+        // VisualElements objects can contain other VisualElement following a tree hierarchy.
+        VisualElement label = new Label("Hello World! From C#");
+        root.Add(label);
+    }
+}
